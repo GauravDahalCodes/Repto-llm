b5e0a099d4bc466071987f497120b524501100af
Gaurav Dahal <gauravdahal121@gmail.com>
2025-05-27 09:50:46 +0545

    Update vite.config.js for GitHub Pages and rebuild

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Repto-Ilm/', // Add this line
   server: {
     port: 3000 // Optional: specify a port
   }
